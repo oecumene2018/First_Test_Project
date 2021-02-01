d1c0f0ef63708943085b006dbd3a2ad91fae46a0
oecumene2018 <76960224+oecumene2018@users.noreply.github.com>
2021-02-01 19:41:35 +0200

    Updated MainPage subclass and test_main_page.py
    Updated MainPage subclass (main_page.py):
    - added should_be_login_link method (with invalid selector on intention)
    Updated test_main_page.py with a new testcase:
    def test_guest_should_see_login_link

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,6 +3,7 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
 
+
 class MainPage(BasePage):
     """Class MainPage inherits from parent (super) class BasePage."""
 
@@ -11,3 +12,8 @@ class MainPage(BasePage):
         Self=BasePage points at a class instance used."""
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        """The method checks that there is a login page link on the Main page."""
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
+

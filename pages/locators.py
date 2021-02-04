c584340388b4dbb6bd29bbdd71b02bc92fa6a5cc
oecumene2018 <76960224+oecumene2018@users.noreply.github.com>
2021-02-04 13:25:16 +0200

    Added login_page.py
    Introduced LoginPage subclass and subclass methods
    should_be_login_url
    should_be_login_form
    should_be_register_form

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,9 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    REGISTRATION_FORM = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_PAGE_URL = "http://selenium1py.pythonanywhere.com/accounts/login/"

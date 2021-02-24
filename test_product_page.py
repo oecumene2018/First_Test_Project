5984b41eda09cab084a5c432fc2e51e502bcc878
oecumene2018 <76960224+oecumene2018@users.noreply.github.com>
2021-02-24 20:22:52 +0200

    Updated pytest_product_page.py
    Uncommented @pytest.mark.parametrize

--- test_product_page.py
+++ test_product_page.py
@@ -8,26 +8,18 @@ from .pages.locators import ProductPageLocators, LoginPageLocators
 link = ProductPageLocators.PRODUCT_PAGE_TEST_LOGIN_URL
 
 
-# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-#                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
-#                                                marks=pytest.mark.xfail(reason="Wrong book name in basket")),
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 @pytest.mark.need_review
-def test_guest_can_add_product_to_basket(browser):
+@pytest.mark.parametrize('offer', ["0", "1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail),
+                                   "8", "9"])
+def test_guest_can_add_product_to_basket(browser, offer):
     # browser.delete_all_cookies()
-    page = ProductPage(browser, link)
+    offer_link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{offer}'
+    page = ProductPage(browser, offer_link)
     page.open()
     add_button = browser.find_element(*ProductPageLocators.ADD_PRODUCT_BUTTON)
     add_button.click()
-    # page.solve_quiz_and_get_code()
-    # browser.implicitly_wait(10)
+    page.solve_quiz_and_get_code()
+    browser.implicitly_wait(10)
     page.should_be_message_product_added_to_basket()
     page.should_be_same_price_page_and_basket()
 
